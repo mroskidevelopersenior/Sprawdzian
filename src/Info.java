f53823c5304fb3394d87ae91d6ee25106bdec6c0
mroskidevelopersenior <oskigaming666@gmail.com>
2024-10-18 11:25:51 +0200

    Dodanie klasy school

--- src/Info.java
+++ src/Info.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class Info {
     public static void main(String[] args) {
 
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Info dla użytkownika");
 //        na branchu 'twoje imię' popraw następna linijkę dopisując nazwę miejscowości w której mieszkasz
         System.out.println("Jestem uczniem Zespołu Szkół im. ks. Stanisława Staszica w Tarnobrzegu. Mieszkam w Orliskach ");
 //        na branchu 'twoje imię' w następnej linii dopisz w jaki sposób dochodzisz/dojeżdżasz do szkoły
         System.out.println("Dojezdzam busem jotka ");
+
+        String School = scanner.nextLine();
+        System.out.println("Podaj szkole");
+
     }
 }
